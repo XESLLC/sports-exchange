c31bd567bfae7f796afd873a7749cdbb57e60c60
David Abramowitz <david.xesllc@gmail.com>
2022-02-25 14:04:12 -0700

    refactor

--- src/server/util/sendEmail.js
+++ src/server/util/sendEmail.js
@@ -5,34 +5,34 @@ const client = new SESClient({ region: REGION });
 
 exports.sendEmail = async(emailAddress, notificationEnum, message) => {
     const params = {
-      Destination: {
-        ToAddresses: [
-          emailAddress
-        ],
-      },
-      Message: {
-        Body: {
-          Html: {
-            Charset: "UTF-8",
-            Data: message,
-          },
+        Destination: {
+            ToAddresses: [
+                emailAddress
+            ],
         },
-        Subject: {
-          Charset: "UTF-8",
-          Data: notificationEnum,
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: message,
+                },
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: notificationEnum,
+            },
         },
-      },
-      Source: "no-reply@fantasysportsstockexchange.com", // SENDER_ADDRESS
-      ReplyToAddresses: ["no-reply@fantasysportsstockexchange.com"],
+        Source: "no-reply@fantasysportsstockexchange.com", // SENDER_ADDRESS
+        ReplyToAddresses: ["no-reply@fantasysportsstockexchange.com"],
     };
 
     const command = new SendEmailCommand(params);
 
     try {
-      const response = await client.send(command);
+        const response = await client.send(command);
     } catch (error) {
-      console.log(error)
+        console.log(error)
     } finally {
-      // finally.
+        // finally.
     }
 };
